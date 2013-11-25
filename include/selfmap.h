d4d77c0faab5581fc05cca726fdfe3f05f15a523
emeryberger <emery@cs.umass.edu>
2013-11-24 19:41:39 -0500

    More cleanup.

--- include/selfmap.h
+++ include/selfmap.h
@@ -29,6 +29,7 @@
 #ifndef DOUBLETAKE_SELFMAP_H
 #define DOUBLETAKE_SELFMAP_H
 
+#include <assert.h>
 #include <linux/limits.h>
 #include <sys/resource.h>
 #include <iostream>
@@ -39,7 +40,6 @@
 //#include <libunwind-ptrace.h>
 //#include <sys/ptrace.h>
 
-#define MAX_BUF_SIZE 4096
 #include "xdefines.h"
 using namespace std;
 
@@ -49,53 +49,63 @@ struct regioninfo {
 };
 
 class selfmap {
+private:
+
+  /// @brief Check whether an address is inside the DoubleTake library itself.
+  bool isDoubleTakeLibrary (void * pcaddr) {
+    return ((pcaddr >= _doubletakeStart) && (pcaddr <= _doubletakeEnd));
+  }
+
+  /// @brief Check whether an address is inside the main application.
+  bool isApplication(void * pcaddr) {
+    return ((pcaddr >= _appTextStart) && (pcaddr <= _appTextEnd));
+  }
+
+  // A class that represents process information, extracted from the maps file.
+  class pmap {
+  public:
+
+    /// @brief Extract information from one line of a /proc/<pid>/maps file.
+    pmap (const char * str)
+    {
+      sscanf (str, "%lx-%lx %4s %lx %x:%x %u %s",
+	      &startaddr, &endaddr, prot, &offset, &dev1, &dev2, &inode, file);
+    }
+  
+    size_t startaddr, endaddr, offset, inode;
+    char file[PATH_MAX];
+    char prot[15];
+    unsigned int dev1, dev2;
+  };
 
 public:
+
   static selfmap& getInstance() {
     static char buf[sizeof(selfmap)];
     static selfmap * theOneTrueObject = new (buf) selfmap();
     return *theOneTrueObject;
   }
 
-  // We may use an inode attribute to analyze whether we need to do this.
-  void getRegionInfo (std::string & curentry,
-		      void ** start,
-		      void ** end) 
+  // Print out the code information about an eip address.
+  // Also try to print out the stack trace of given pcaddr.
+  void printCallStack (ucontext_t * context, void * addr, bool isOverflow);
+ 
+  /// @brief Get information about global regions. 
+  void getTextRegions() 
   {
-
-    // Now this entry is about globals of libc.so.
-    string::size_type pos = 0;
-    string::size_type endpos = 0;
-    // Get the starting address and end address of this entry.
-    // Normally the entry will be like this
-    // "00797000-00798000 rw-p ...."
-    string beginstr, endstr;
-
-    while (curentry[pos] != '-') 
-      pos++;
-
-    beginstr = curentry.substr(0, pos);
-  
-    // Skip the '-' character
-    pos++;
-    endpos = pos;
-    
-    // Now pos should point to a space or '\t'.
-    while(!isspace(curentry[pos])) pos++;
-    endstr = curentry.substr(endpos, pos-endpos);
-
-    // Save this entry to the passed regions.
-    *start = (void *)strtoul(beginstr.c_str(), NULL, 16);
-    *end = (void *)strtoul(endstr.c_str(), NULL, 16);
-
-    return;
-  }
-  
-  // @brief Get information about global regions. 
-  void getTextRegions() {
     ifstream iMapfile;
     string curentry;
 
+    // Get application file name.
+    int count = Real::readlink()("/proc/self/exe", _filename, PATH_MAX);
+    if (count <= 0 || count >= PATH_MAX)
+    {
+      PRWRN("Failed to get current executable file name\n" );
+      exit(1);
+    }
+    _filename[count] = '\0';
+
+    // Read the maps to compute ranges for text regions (code).
     try {
       iMapfile.open("/proc/self/maps");
     } catch(...) {
@@ -104,114 +114,84 @@ public:
     } 
 
     // Now we analyze each line of the map file.
-    void * startaddr, * endaddr;
-    bool   hasLibStart = false;
-    _appTextStart = (void *) (-1ULL);
-    _appTextEnd = (void *) 0ULL;
+    _appTextStart = (void *) (-1ULL);  // largest possible address
+    _appTextEnd   = (void *) 0ULL;     // smallest possible address
+    _libraryStart    = _appTextStart;
+    _libraryEnd      = _appTextEnd;
 
     printf ("start = %lx, end = %x\n", _appTextStart, _appTextEnd);
 
     while (getline(iMapfile, curentry)) {
-      size_t start, end, offset, inode;
-      char file[PATH_MAX];
-      char prot[15];
-      unsigned int dev1, dev2;
-      printf ("Read in %s\n", curentry.c_str());
-      int count = sscanf (curentry.c_str(), "%lx-%lx %4s %lx %x:%x %u %s",
-			  &start, &end, prot, &offset, &dev1, &dev2, &inode, file);
-      if (count != 0) {
-	printf ("start = %lx, end = %lx, prot = %s, offset = %lx, dev1 = %x, dev2 = %x, inode = %x, file = %s\n",
-		start, end, prot, offset, dev1, dev2, inode, file);
-      }
+
+      pmap p (curentry.c_str());
 
       // Check whether this entry is the text segment of application.
-      if((curentry.find(" r-xp ", 0) != string::npos) 
-	 && (curentry.find(" 08:0", 0) != string::npos)) {
-
+      if (strcmp (p.prot, "r-xp") == 0) {
+	// We're in.
 	printf ("checking text segment.\n");
-        getRegionInfo(curentry, &startaddr, &endaddr);
 
-        // Check whether this is doubletake or application
-        if (curentry.find("libdoubletake", 0) != string::npos) {
-          _doubletakeStart = startaddr;
-          _doubletakeEnd = endaddr;
-          break;
-        } else {
-	  printf ("CHECKING.\n");
-	  if ((size_t) startaddr < (size_t) _appTextStart) {
-	    _appTextStart = startaddr;
+        // Check whether we are in DoubleTake, another library, or in the application itself.
+        if (strstr(p.file, "libdoubletake") != NULL) {
+          _doubletakeStart = p.startaddr;
+          _doubletakeEnd =   p.endaddr;
+        } else if (strcmp(p.file, _filename) == 0) {
+	  if (p.startaddr < (size_t) _appTextStart) {
+	    _appTextStart = p.startaddr;
 	  }
-	  if ((size_t) endaddr > (size_t) _appTextEnd) {
-	    _appTextEnd = endaddr;
+	  if (p.endaddr > (size_t) _appTextEnd) {
+	    _appTextEnd = p.endaddr;
 	  }
-        } 
-      } else if((curentry.find(" r-xp ") != string::npos) 
-              && (curentry.find("lib", 0) != string::npos) 
-              && (curentry.find(" 08:0") != string::npos)) {
-        // Now it is start of global of applications
-        getRegionInfo(curentry, &startaddr, &endaddr);
-
-        if (!hasLibStart) {
-          _libraryStart = startaddr;
-          _libraryEnd = endaddr;
-          hasLibStart = true;
         } else {
-          _libraryEnd = endaddr;
-        }
+	  // Must be in a library.
+	  if (p.startaddr < (size_t) _libraryStart) {
+	    _libraryStart = p.startaddr;
+	  }
+	  if (p.endaddr > (size_t) _appTextEnd) {
+	    _libraryEnd = p.endaddr;
+	  }
       }
     }
+
     iMapfile.close();
-
-    printf ("NOW: start = %lx, end = %lx\n", _appTextStart, _appTextEnd);
-
-    int    count;
-
-    /* Get current executable file name. */
-    count = Real::readlink()("/proc/self/exe", _filename, MAX_BUF_SIZE);
-    if (count <= 0 || count >= MAX_BUF_SIZE)
-    {
-      PRWRN("Failed to get current executable file name\n" );
-      exit(1);
-    }
-    _filename[count] = '\0';
 
     PRINF("INITIALIZATION: textStart %p textEnd %p doubletakeStart %p doubletakeEnd %p libStart %p libEnd %p\n", _appTextStart, _appTextEnd, _doubletakeStart, _doubletakeEnd, _libraryStart, _libraryEnd);
   } 
 
-  // Check whether an address is inside the doubletake library itself
-  bool isDoubleTakeLibrary(void * pcaddr) {
-    if(pcaddr >= _doubletakeStart && pcaddr <= _doubletakeEnd) {
-      return true;
+  /// @brief Extract stack bottom and top information.
+  void getStackInformation(void** stackBottom, void ** stackTop) {
+    using namespace std;
+    ifstream iMapfile;
+    string curentry;
+
+    try {
+      iMapfile.open("/proc/self/maps");
+    } catch(...) {
+      PRWRN("can't open /proc/self/maps, exiting now.\n");
+      abort();
+    } 
+
+    *stackbottom = NULL;
+    *stacktop = NULL;
+
+    // Now we analyze each line of this maps file, looking for the stack.
+    while (getline(iMapfile, curentry)) {
+
+      pmap p (curentry.c_str());
+
+      // Find the entry for the stack.
+      if (strstr(p.file, "[stack]") != NULL) {
+	*stackBottom = p.startaddr;
+	*stackTop    = p.endaddr;
+	// Got it. We're out.
+	break;
+      }
     }
-    else {
-      return false;
-    }
+    iMapfile.close();
+    assert (*stackBottom != NULL);
+    assert (*stackTop != NULL);
   }
 
-  bool isApplication(void * pcaddr) {
-    if(pcaddr >= _appTextStart && pcaddr <= _appTextEnd) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-
-  /* Get the stack frame inside the tracer.
-   * We can not simply use backtrace to get corresponding information
-   * since it will give us the call stack of signal handler.
-   * Not the watching processes that we need.
-   * According to http://stackoverflow.com/questions/7258273/how-to-get-a-backtrace-like-gdb-using-only-ptrace-linux-x86-x86-64,
-   * The following link can give us some clues to use libunwind.
-    http://git.savannah.gnu.org/cgit/libunwind.git/plain/tests/test-ptrace.c?h=v1.0-stable
-    http://git.savannah.gnu.org/cgit/libunwind.git/plain/tests/test-ptrace.c?h=v1.0-stable
-
-   */
-  // Print out the code information about an eipaddress
-  // Also try to print out stack trace of given pcaddr.
-  void printCallStack(ucontext_t * context, void * addr, bool isOverflow);
- 
-  // Trying to get information about global regions. 
+  /// @brief Collect all global regions.
   void getGlobalRegions(regioninfo * regions, int * regionNumb) {
     using namespace std;
     ifstream iMapfile;
@@ -223,7 +203,7 @@ public:
     try {
       iMapfile.open("/proc/self/maps");
     } catch(...) {
-      PRWRN("can't open /proc/self/maps, exit now\n");
+      PRWRN("can't open /proc/self/maps, exiting.\n");
       abort();
     } 
 
@@ -231,18 +211,19 @@ public:
     bool toExit = false;
     bool toSaveRegion = false;
  
-    void * startaddr, * endaddr;
     string nextentry;
     void * newstart;
     bool foundGlobals = false;
 
-    while(getline(iMapfile, curentry)) {
-      // Check the globals for the application. It is at the first entry
-      if(foundGlobals == false && (curentry.find(" rw-p ") != string::npos)) {
-        // Now it is start of global of applications
-        getRegionInfo(curentry, &startaddr, &endaddr);
+    while (getline(iMapfile, curentry)) {
+
+      pmap p (curentry.c_str());
+
+      // Check the globals for the application. It is the first entry.
+      if ((foundGlobals == false) && (strstr(p.prot, "rw-p") != NULL)) {
+        // Now it is start of global of applications.
    
-        getline(iMapfile, nextentry);
+        getline (iMapfile, nextentry);
 
         void * newstart;
         // Check whether next entry should be also included or not.
@@ -283,56 +264,13 @@ public:
     iMapfile.close();
   } 
   
-  // Trying to get stack information. 
-  void getStackInformation(void** stackBottom, void ** stackTop) {
-    using namespace std;
-    ifstream iMapfile;
-    string curentry;
-
-    try {
-      iMapfile.open("/proc/self/maps");
-    } catch(...) {
-      PRWRN("can't open /proc/self/maps, exit now\n");
-      abort();
-    } 
-
-    // Now we analyze each line of this maps file.
-    while(getline(iMapfile, curentry)) {
-
-      // Find the entry for stack information.
-      if(curentry.find("[stack]", 0) != string::npos) {
-        string::size_type pos = 0;
-        string::size_type endpos = 0;
-        // Get the starting address and end address of this entry.
-        // Normally the entry will be like this
-        // ffce9000-ffcfe000 rw-p 7ffffffe9000 00:00 0   [stack]
-        string beginstr, endstr;
-
-        while(curentry[pos] != '-') pos++;
-          beginstr = curentry.substr(0, pos);
-  
-          // Skip the '-' character
-          pos++;
-          endpos = pos;
-        
-          // Now pos should point to a space or '\t'.
-          while(!isspace(curentry[pos])) pos++;
-          endstr = curentry.substr(endpos, pos-endpos);
-
-          // Now we get the start address of stack and end address
-          *stackBottom = (void *)strtoul(beginstr.c_str(), NULL, 16);
-          *stackTop = (void *)strtoul(endstr.c_str(), NULL, 16);
-      }
-    }
-    iMapfile.close();
-  }
 
 private:
-  char _filename[MAX_BUF_SIZE];
+  char _filename[PATH_MAX];
   void * _appTextStart;
   void * _appTextEnd;
   void * _libraryStart;
-  void * _libraryEnd;
+  void * _libraryEnd; 
   void * _doubletakeStart;
   void * _doubletakeEnd; 
 };
