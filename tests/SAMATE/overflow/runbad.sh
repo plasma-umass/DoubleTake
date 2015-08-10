789dd15b34daa080b96da4d44031b7a5cd6747a4
tongping <Tongping.Liu@utsa.edu>
2015-08-10 12:36:11 -0400

    Change the scritps to use doubletake instead of dthread

--- tests/SAMATE/overflow/runbad.sh
+++ tests/SAMATE/overflow/runbad.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-#./Heap_overflow_15-dthread
-#./heap_overflow_cplx_1845-dthread
-#./HeapOverFlow_ArrayIndex_bad1_2065-dthread
-#./HeapOverFlow_bad_1952-dthread
-#./HeapOverFlow_bad_2062-dthread
-#./HeapOverFlow_ArrayIndex_bad_1961-dthread
-#./heap_overflow_array_953-dthread mybadtest
-#./heap_overflow_cplx_2145-dthread
-#./heap_overflow_location_2147-dthread
-#./HeapOverFlow_ArrayAddress_bad_1958-dthread mybadtestnow
-#./HeapOverFlow_ArrayAddress_bad_2064-dthread mybadtestnow
-./HeapOverFlow_Scope_bad_1955-dthread mybadtestnow
+#./Heap_overflow_15-doubletake
+#./heap_overflow_cplx_1845-doubletake
+#./HeapOverFlow_ArrayIndex_bad1_2065-doubletake
+#./HeapOverFlow_bad_1952-doubletake
+#./HeapOverFlow_bad_2062-doubletake
+#./HeapOverFlow_ArrayIndex_bad_1961-doubletake
+#./heap_overflow_array_953-doubletake mybadtest
+#./heap_overflow_cplx_2145-doubletake
+#./heap_overflow_location_2147-doubletake
+#./HeapOverFlow_ArrayAddress_bad_1958-doubletake mybadtestnow
+#./HeapOverFlow_ArrayAddress_bad_2064-doubletake mybadtestnow
+./HeapOverFlow_Scope_bad_1955-doubletake mybadtestnow
 
 # Problems
-#./HeapOverFlow_Scope_bad_2063-dthread
+#./HeapOverFlow_Scope_bad_2063-doubletake
 
 
 
--- tests/SAMATE/overflow/rungood.sh
+++ tests/SAMATE/overflow/rungood.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-./heap_overflow_array_good_959-dthread
-echo "heap_overflow_array_good_959-dthread";
-./heap_overflow_basic_good_1936-dthread 16
-./heap_overflow_cplx_good_1846-dthread
-./heap_overflow_cplx_good_2149-dthread
-./HeapOverFlow_good_2134-dthread
-./heap_overflow_location_good_1848-dthread
-./heap_overflow_location_good_2148-dthread
-./HeapOverflow_ArrayAddress_good_1959-dthread
-./HeapOverflow_ArrayAddress_good_2068-dthread
-./HeapOverflow_ArrayIndex_good_1962-dthread
-./HeapOverflow_good_1953-dthread
-./HeapOverflow_good_2066-dthread
-./HeapOverflow_Scope_good_1956-dthread
-./HeapOverflow_Scope_good_2067-dthread
+./heap_overflow_array_good_959-doubletake
+echo "heap_overflow_array_good_959-doubletake";
+./heap_overflow_basic_good_1936-doubletake 16
+./heap_overflow_cplx_good_1846-doubletake
+./heap_overflow_cplx_good_2149-doubletake
+./HeapOverFlow_good_2134-doubletake
+./heap_overflow_location_good_1848-doubletake
+./heap_overflow_location_good_2148-doubletake
+./HeapOverflow_ArrayAddress_good_1959-doubletake
+./HeapOverflow_ArrayAddress_good_2068-doubletake
+./HeapOverflow_ArrayIndex_good_1962-doubletake
+./HeapOverflow_good_1953-doubletake
+./HeapOverflow_good_2066-doubletake
+./HeapOverflow_Scope_good_1956-doubletake
+./HeapOverflow_Scope_good_2067-doubletake
 
