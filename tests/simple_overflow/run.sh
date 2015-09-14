c3f3b2dfcf6a5604afaeb6b47957c5d764f56bad
Bobby Powers <bobbypowers@gmail.com>
2015-09-14 17:17:04 -0400

    build: non-recursive Makefile

--- tests/simple_overflow/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-LD_LIBRARY_PATH=/nfs/cm/scratch1/tonyliu/grace/branches/DoubleTake/DoubleTakeCharlie/DoubleTake ./simple_overflow
--- tests/simple_uaf/main.c
+++ tests/simple_uaf/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define USED(v) ((void)(v))
+
 int main(int argc, char** argv) {
   int* p = (int*)malloc(sizeof(int));
   *p = 123;
@@ -8,6 +10,7 @@ int main(int argc, char** argv) {
   free(p);
   for (int i = 0; i < 1000; i++) {
     volatile int q = 1.0 * 1.0;
+    USED(q);
   }
   *p = 456;
   return 0;
