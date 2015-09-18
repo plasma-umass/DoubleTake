2222d446f387fa59a5b25c324d2cb189d09a7350
Bobby Powers <bobbypowers@gmail.com>
2015-09-18 09:48:42 -0400

    tests/simple_mt_uaf: don't use stdatomic, clang is braindead
    Clang, as shipped on Debian and Ubuntu and Fedora, fails so hard when
    you include stdatomic.h.  It loads the GCC libstd++ headers, which
    conflict with its own atomic crap, and then dies.  Just use GCC atomic
    builtins, hopefully these are supported better.

--- tests/simple_mt_uaf/main.c
+++ tests/simple_mt_uaf/main.c
@@ -1,5 +1,4 @@
 #include <pthread.h>
-#include <stdatomic.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -62,8 +61,8 @@
 // sequence number - use this to coordinate memory operation orders
 // without mutexes (so that coordination between threads doesn't
 // trigger epoch expirations)
-atomic_int       g_seq;
-atomic_uintptr_t g_array;
+int       g_seq;
+uintptr_t g_array;
 
 // release the cacheline on the variable we're waiting for
 static inline void spin() {
@@ -76,20 +75,20 @@ worker_main(void *arg) {
 
 	// wait for the main thread to store the dynamically allocated
 	// array in g_array
-	while (atomic_load_explicit(&g_seq, memory_order_acquire) != 1)
+	while (__atomic_load_n(&g_seq, __ATOMIC_ACQUIRE) != 1)
 		spin();
 
 	// get the location of the array, write to it, and free it.
-	char *array = (char *)atomic_load(&g_array);
+	char *array = (char *)__atomic_load_n(&g_array, __ATOMIC_SEQ_CST);
 	for (size_t i = 0; i < ARRAY_SIZE; i++)
 		array[i] = n;
 	free(array);
 
 	// inform the main thread that we have free'd the array
-	atomic_store_explicit(&g_seq, 2, memory_order_release);
+	__atomic_store_n(&g_seq, 2, __ATOMIC_RELEASE);
 
 	// wait for main thread to perform a use-after-free before we exit
-	while (atomic_load_explicit(&g_seq, memory_order_acquire) != 3)
+	while (__atomic_load_n(&g_seq, __ATOMIC_ACQUIRE) != 3)
 		spin();
 
 	//pthread_exit(NULL);
@@ -109,12 +108,12 @@ main(int argc, const char *argv[]) {
 	// pass the array to the worker thread, and increase the
 	// sequence number, allowing the worker to progress into the
 	// 'write + free' phase.
-	atomic_thread_fence(memory_order_seq_cst);
-	atomic_store(&g_array, (uintptr_t)array);
-	atomic_store(&g_seq, 1);
+	__atomic_thread_fence(__ATOMIC_SEQ_CST);
+	__atomic_store_n(&g_array, (uintptr_t)array, __ATOMIC_SEQ_CST);
+	__atomic_store_n(&g_seq, 1, __ATOMIC_SEQ_CST);
 
 	// wait for the worker to free the array
-	while (atomic_load_explicit(&g_seq, memory_order_acquire) != 2)
+	while (__atomic_load_n(&g_seq, __ATOMIC_ACQUIRE) != 2)
 		spin();
 
 	// at this point, the array has been free'd in the worker
@@ -122,7 +121,7 @@ main(int argc, const char *argv[]) {
 	array[0] = 0;
 
 	// tell the worker it can exit
-	atomic_store(&g_seq, 3);
+	__atomic_store_n(&g_seq, 3, __ATOMIC_SEQ_CST);
 
 	// wait for the worker to exit
 	pthread_join(worker, NULL);
